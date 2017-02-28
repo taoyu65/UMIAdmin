226e7ebd39b396bb3e09f61673c735ddd7acf390
taoyu65 <taoyu65@gmail.com>
2017-02-28 01:34:52 -0800

    working on style of side menu
    1. the proper style is showing on the right menu after you click one
    menu (not for super admin that is still working on it)

--- src/Umi/Menus.php
+++ src/Umi/Menus.php
@@ -18,8 +18,8 @@ class Menus
     #super admin can get all menus
     public function AllMenus()
     {
-        $html = $this->dashboard();
-        $html .= $this->recursionAllMenus(0);
+        //$html = $this->dashboard();
+        $html = $this->recursionAllMenus(0);
         return $html;
     }
 
@@ -27,21 +27,51 @@ class Menus
     #get part of menus according to the authorization
     public function Menus($json)
     {
-        $html = $this->dashboard();
+        $html = '';//$this->dashboard();
         try {
             $jsonMenus = json_decode($json);
-            $html .= $this->recursionPartMenus($jsonMenus);
+
+            #获取 active 或者 open的样式 并且以id标识
+            #get menu's css right(proper style) and use id as a identity
+            $menuLevel = $this->activeOrOpenStyle($jsonMenus);
+
+            $html .= $this->recursionPartMenus($jsonMenus, $menuLevel);
             return $html;
         } catch (Exception $e) {
             throw $e;
         }
     }
 
-    private function dashboard()
+    #根据菜单的深度路径(一个菜单的所有父类) 计算并作为数组返回
+    #return a array of menu style according to the menu's deep path(all the menu's parents)
+    private function activeOrOpenStyle($jsonMenus)
+    {
+        $activeOrOpen = [];
+        if (array_key_exists('id', $_REQUEST) && is_numeric($_REQUEST['id'])) {
+            $menuLevel = $this->getLevelOfMenu($jsonMenus, $_REQUEST['id']);
+            if (count($menuLevel) == 1) {
+                $activeOrOpen[$menuLevel[0]] = 'active';
+            } else {
+                $count = count($menuLevel);
+                for ($i = 0; $i < $count; $i++) {
+                    if ($i == 0) {
+                        $activeOrOpen[$menuLevel[$i]] = 'active open';
+                    } elseif ($i == $count - 1) {
+                        $activeOrOpen[$menuLevel[$i]] = 'active';
+                    } else {
+                        $activeOrOpen[$menuLevel[$i]] = 'open';
+                    }
+                }
+            }
+        }
+        return $activeOrOpen;
+    }
+
+    protected function dashboard()
     {
         $dashboard = route('dashboard');
         $html = <<<EOD
-        <li class="active">
+        <li class="" id="dashboard">
 		    <a href="$dashboard">
 			    <i class="menu-icon fa fa-tachometer"></i>
 				<span class="menu-text"> Dashboard </span>
@@ -52,7 +82,9 @@ EOD;
         return $html;
     }
 
-    private function recursionPartMenus($jsonMenus, $levelInit = 0)
+    #根据用户自定义菜单的json加载
+    #load menu by json that is related to user
+    protected function recursionPartMenus($jsonMenus, $menuLevel, $levelInit = 0)
     {
         $html = '';
         $level = 0;
@@ -60,10 +92,16 @@ EOD;
         foreach ($jsonMenus as $jsonMenu) {
             $objMenu = $this->menus->getOneMenu($jsonMenu->id);
             $rootMenu = $level == 0 ? '<span class="menu-text">' . $objMenu->title . '</span>' : $objMenu->title;
+            //menus class(active or open) -------------------------------------------
+            $url = $objMenu->url === '#' ? '#' : $objMenu->url . '?id=' . $objMenu->id;
+            $class = '';
+            if(array_key_exists($objMenu->id, $menuLevel))
+                $class = $menuLevel[$objMenu->id];
+            //-----------------------------------------------------------------------
             if (array_key_exists('children', $jsonMenu)){
                 $html .= <<<EOD
-                <li class="">
-                    <a href="$objMenu->url" target="$objMenu->target" class="dropdown-toggle">
+                <li class='$class'>
+                    <a href="$url" target="$objMenu->target" class="dropdown-toggle">
 			            <i class="menu-icon fa $objMenu->icon_class"></i>
 			            $rootMenu
 			            <b class="arrow fa fa-angle-down"></b>
@@ -71,13 +109,13 @@ EOD;
                     <b class="arrow"></b>
 EOD;
                 $html .= '<ul class="submenu">';
-                $html .= $this->recursionPartMenus($jsonMenu->children, 1);
+                $html .= $this->recursionPartMenus($jsonMenu->children, $menuLevel, 1);
                 $html .= '</ul>';
                 $html .= '</li>';
             } else {
                 $html .= <<<EOD
-                <li class="">
-                    <a href="$objMenu->url" target="$objMenu->target">
+                <li class='$class'>
+                    <a href="$url" target="$objMenu->target">
 			            <i class="menu-icon fa $objMenu->icon_class"></i>
 			        $rootMenu
 		            </a>
@@ -90,16 +128,22 @@ EOD;
         return $html;
     }
 
-    private function recursionAllMenus($menu_id = 0)
+    protected function recursionAllMenus($menu_id = 0)
     {
         $menus = $this->menus->getMenus($menu_id);
         $html = '';
         foreach ($menus as $menu) {
             $rootMenu = $menu_id == 0 ? '<span class="menu-text">' . $menu->title . '</span>' : $menu->title;
+            //menus class(active or open) -------------------------------------------
+            $url = $menu->url === '#' ? '#' : $menu->url . '?id=' . $menu->id;
+            $class = '';
+            //if(array_key_exists($menu->id, $menuLevel))
+              //  $class = $menuLevel[$menu->id];
+            //-----------------------------------------------------------------------
             if ($this->menus->isSubMenu($menu->id)) {
                 $html .= <<<EOD
-                <li class="">
-                    <a href="$menu->url" target="$menu->target" class="dropdown-toggle">
+                <li class="$class">
+                    <a href="$url" target="$menu->target" class="dropdown-toggle">
 			            <i class="menu-icon fa $menu->icon_class"></i>
 			            $rootMenu
 			            <b class="arrow fa fa-angle-down"></b>
@@ -112,8 +156,8 @@ EOD;
                 $html .= '</li>';
             } else {
                 $html .= <<<EOD
-                <li class="">
-                    <a href="$menu->url" target="$menu->target">
+                <li class="$class">
+                    <a href="$url" target="$menu->target">
 			            <i class="menu-icon fa $menu->icon_class"></i>
 			        $rootMenu
 		            </a>
@@ -123,5 +167,35 @@ EOD;
             }
         }
         return $html;
+    }
+
+    /** use for showing the side menu's style, remaining the active menu's style after refresh page
+     * @param $jsonMenus - type is array.
+     * @param $id - id from table umi_menus
+     * @return array - deep path (all the parents id of table umi_menus)
+     */
+    private function getLevelOfMenu($jsonMenus, $id)
+    {
+        $arrString = $this->recursionJsonMenu($jsonMenus, $id);
+        return explode(',', trim($arrString, ','));
+    }
+
+    private function recursionJsonMenu($jsonMenus, $id, $returnString = '')
+    {
+        $temString = '';
+        foreach ($jsonMenus as $jsonMenu) {
+            if (strstr($temString, $id . ','))
+                return $temString;
+            $temString = $returnString . $jsonMenu->id . ',';
+            #recursion
+            if (array_key_exists('children', $jsonMenu)) {
+                $temString = $this->recursionJsonMenu($jsonMenu->children, $id, $temString);
+            } else {
+                if ($id == $jsonMenu->id) {
+                    return $temString;
+                }
+            }
+        }
+        return $temString;
     }
 }
