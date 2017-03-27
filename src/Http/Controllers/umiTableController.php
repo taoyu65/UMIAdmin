07f5291aa91dfeffbf014a6305c0ea39b1e11a39
taoyu65 <taoyu65@gmail.com>
2017-03-26 21:47:34 -0700

    this controller page is for testing

--- src/Http/Controllers/umiTableController.php
+++ src/Http/Controllers/umiTableController.php
@@ -3,8 +3,10 @@
 namespace YM\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Gate;
 use YM\Umi\FactoryBreadBrowser;
 use YM\umiAuth\Facades\umiAuth;
+use YM\umiAuth\src\Models\Role;
 
 class umiTableController extends Controller
 {
@@ -17,13 +19,32 @@ class umiTableController extends Controller
         foreach ($b as $bb) {
             //var_dump($bb->key);
         }*/
-
-        //var_dump(Auth::user()->can('update-umi_table', $post, true));
-        var_dump(umiAuth::hasPermission('add-menu_role'));
+        //var_dump($tableName);
+//        var_dump(umiAuth::cannot('*-menu_role'));
+//        var_dump(umiAuth::can('delete-menu_role'));
+//        var_dump(umiAuth::can(['delete-menu_role','*-menu_role'], true));
+//        var_dump(umiAuth::hasRole([1,2], true));
+//        var_dump(umiAuth::ability(['table_user','article_user'],['delete-menu_role','edit-menu_role'],['validate_all' => true, 'return_type' => 'both']));
+        //$user = new User();
+        /*$role = new Role();
+        $role->role_name = 'newrocwlke111';
+        $role->display_name = 'displaynew';
+        try {
+            $role->save();
+        } catch (\Exception $e) {
+            //print_r('<script>alert("Role name already exist.")</script>');
+        }*/
+        /**/
+        //dd($user->roles()->getResults());
+        //dd(Auth::user()->ability('admin,owner', 'create-post,edit-user'));
+        //$user->find(1)->roles()->attach(12);
+//        umiAuth::user();
+        //umiAuth::attach($role);
+        //umiAuth::detach([32,33]);
         //
         if ($tableName == '') return view('umi::umiTableAll');
 
-        $factoryBread = new FactoryBreadBrowser();
+        $factoryBread = new FactoryBreadBrowser($tableName);
         $breadBrowser = $factoryBread->getBreadBrowser();
         $header = $breadBrowser->header();
         $tableBody = $breadBrowser->tableBody();
