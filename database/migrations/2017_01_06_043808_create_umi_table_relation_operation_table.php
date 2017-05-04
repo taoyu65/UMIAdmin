3951a419064beef6a10abddc42a8c6d3a760429b
Tao <taoyu65@gmail.com>
2017-05-03 19:39:32 -0700

    migration file's wrong
    1. wrong file's name

--- database/migrations/2017_01_06_043808_create_umi_table_relation_operation_table.php
+++ database/migrations/2017_01_06_043808_create_umi_table_relation_operation_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableRelationOperationTable extends Migration
+class CreateUmiTableRelationOperationTable extends Migration
 {
     /**
      * Run the migrations.
